55dc6f039f4ee4053603a3cbe36f41e7390b1c7e
jasoncUD <jasonc@udel.edu>
2023-09-19 09:58:25 -0400

    Added a user login component

--- todo-web/src/app/app.component.ts
+++ todo-web/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog'
+import { UserLoginComponent } from './user-login/user-login.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jasonToDoProject';
+  displayLogin: boolean = false;
+
+  constructor(
+    private dialog: MatDialog
+  ) {
+
+  }
+
+  openUserLogin() {
+    this.dialog.open(UserLoginComponent);
+  }
 }
